5a6c5936a5f4ee10f5cef6450e994bab9ee21c93
Jalaj <Jalaj Pathak>
2022-07-26 19:56:44 +0530

    initial commit

---
+++ src/Requests.js
@@ -0,0 +1,15 @@
+const API_KEY = "298e3eed1042a66c8c894fa26e02b797";
+
+const requests = {
+  fetchTrending: `/trending/all/day?/api_key=${API_KEY}`,
+  fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+  fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+  fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+  fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+  fetchHorroMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+  fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+  fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+  API_KEY: API_KEY,
+};
+
+export default requests;
